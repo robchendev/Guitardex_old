e9364035bdfdd2cbf3f3f5993b34e7aba7825df4
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-03-04 01:04:15 -0800

    setup some styles from backup, some styles still havent been added back.

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -5,26 +5,28 @@ import { Helmet } from "react-helmet"
 import { ThemeProvider } from "styled-components"
 import FontLoad from "../../assets/fonts"
 import { GlobalStyle } from "../../styles/globalStyles"
-import { lightTheme } from "../../styles/theme"
+import { darkTheme, lightTheme } from "../../styles/theme"
 
 // This can also be exported from somewhere else for Sidebar.tsx
 export const ThemeContext = React.createContext(null)
 
 const Layout = (props) => {
+  const [theme, setTheme] = useState("light")
+  const themeStyle = theme === 'light' ? lightTheme : darkTheme
   return (
-
-    <ThemeProvider theme={lightTheme}>
-      <GlobalStyle />
-      <Helmet>
-        <title>{props.title}</title>
-        <FontLoad />
-      </Helmet>
-      <SLayout>
-        <Sidebar />
-        <SMain>{props.children}</SMain>
-      </SLayout>
-    </ThemeProvider>
-
+    <ThemeContext.Provider value={{ setTheme, theme }}>
+      <ThemeProvider theme={lightTheme}>
+        <GlobalStyle />
+        <Helmet>
+          <title>{props.title}</title>
+          <FontLoad />
+        </Helmet>
+        <SLayout>
+          <Sidebar />
+          <SMain>{props.children}</SMain>
+        </SLayout>
+      </ThemeProvider>
+    </ThemeContext.Provider>
   )
 }
 
