ba196c789b4c6cbde3c6bf367641532f926cf9bc
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-03-04 03:02:54 -0800

    added cookie plugin

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -1,26 +1,5 @@
-/**
- * SSR code by Divyanshu Maithani
- * https://divyanshu013.dev/blog/gatsby-dark-mode/
- */
-import { createElement } from "react"
-import Cookies from 'universal-cookie';
-
-const applyDarkModeClass = `
-(function() {
-  try {
-    let mode = cookies.get('theme');
-    if (mode === 'dark') {
-			document.body.classList.add('dark');
-		}
-  } catch (e) {}
-})();
-`;
-
-export const onRenderBody = ({ setPreBodyComponents }) => {
-	const script = createElement('script', {
-		dangerouslySetInnerHTML: {
-			__html: applyDarkModeClass,
-		},
-	});
-	setPreBodyComponents([script]);
+exports.onRenderBody = ({ setBodyAttributes }) => {
+  setBodyAttributes({
+      className: 'no-js'
+    });
 };
