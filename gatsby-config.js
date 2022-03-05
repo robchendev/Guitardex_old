0fbfbe0a3b7fa4e9fb73ccb75c70de1e2722652b
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-03-05 05:45:58 -0800

    added gtag

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,10 +8,31 @@ module.exports = {
   },
   plugins: ["gatsby-remark-embed-video", `gatsby-plugin-netlify`, "gatsby-plugin-emotion", 
   {
-    resolve: 'gatsby-plugin-google-analytics',
+    resolve: `gatsby-plugin-google-gtag`,
     options: {
-      "trackingId": "G-1H3X77R5H1"
-    }
+      // You can add multiple tracking ids and a pageview event will be fired for all of them.
+      trackingIds: [
+        "UA-222098428-1",
+      ],
+      // This object gets passed directly to the gtag config command
+      // This config will be shared across all trackingIds
+      gtagConfig: {
+        optimize_id: "OPT_CONTAINER_ID",
+        anonymize_ip: false,
+        cookie_expires: 0,
+      },
+      // This object is used for configuration specific to this plugin
+      pluginConfig: {
+        // Puts tracking script in the head instead of the body
+        head: false,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: ["/preview/**", "/do-not-track/me/too/"],
+        // Defaults to https://www.googletagmanager.com
+        origin: "YOUR_SELF_HOSTED_ORIGIN",
+      },
+    },
   },
   { // You can have multiple instances of this plugin
     resolve: `gatsby-source-filesystem`,
