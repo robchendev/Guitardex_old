f66685c5f4235047cc084e58d9475ffd95a50993
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-03-31 15:31:13 -0700

    reset gatsby ssr because the looper wasnt working

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -15,10 +15,90 @@ const MagicScriptTag = () => {
     }
     const colorMode = getInitialColorMode();
     const root = document.documentElement;
-    Object.entries(COLORS).forEach(([name, colorByTheme]) => {
-      const cssVarName = \`--color-\${name}\`;
-      root.style.setProperty(cssVarName, colorByTheme[newValue]);
-    });
+    root.style.setProperty(
+      '--color-text',
+      colorMode === 'light'
+        ? '${COLORS.text.light}'
+        : '${COLORS.text.dark}'
+    );
+    root.style.setProperty(
+      '--color-bg',
+      colorMode === 'light'
+        ? '${COLORS.bg.light}'
+        : '${COLORS.bg.dark}'
+    );
+    root.style.setProperty(
+      '--color-bgAlpha',
+      colorMode === 'light'
+        ? '${COLORS.bgAlpha.light}'
+        : '${COLORS.bgAlpha.dark}'
+    );
+    root.style.setProperty(
+      '--color-bg2',
+      colorMode === 'light'
+        ? '${COLORS.bg2.light}'
+        : '${COLORS.bg2.dark}'
+    );
+    root.style.setProperty(
+      '--color-bg3',
+      colorMode === 'light'
+        ? '${COLORS.bg3.light}'
+        : '${COLORS.bg3.dark}'
+    );
+    root.style.setProperty(
+      '--color-primary',
+      colorMode === 'light'
+        ? '${COLORS.primary.light}'
+        : '${COLORS.primary.dark}'
+    );
+    root.style.setProperty(
+      '--color-toggle',
+      colorMode === 'light'
+        ? '${COLORS.toggle.light}'
+        : '${COLORS.toggle.dark}'
+    );
+    root.style.setProperty(
+      '--color-tabimg',
+      colorMode === 'light'
+        ? '${COLORS.tabimg.light}'
+        : '${COLORS.tabimg.dark}'
+    );
+    root.style.setProperty(
+      '--color-link',
+      colorMode === 'light'
+        ? '${COLORS.link.light}'
+        : '${COLORS.link.dark}'
+    );
+    root.style.setProperty(
+      '--color-linkHover',
+      colorMode === 'light'
+        ? '${COLORS.linkHover.light}'
+        : '${COLORS.linkHover.dark}'
+    );
+    root.style.setProperty(
+      '--color-toggleThumb',
+      colorMode === 'light'
+        ? '${COLORS.toggleThumb.light}'
+        : '${COLORS.toggleThumb.dark}'
+    );
+    root.style.setProperty(
+      '--color-toggleName',
+      colorMode === 'light'
+        ? '${COLORS.toggleName.light}'
+        : '${COLORS.toggleName.dark}'
+    );
+    root.style.setProperty(
+      '--color-moonIcon',
+      colorMode === 'light'
+        ? '${COLORS.moonIcon.light}'
+        : '${COLORS.moonIcon.dark}'
+    );
+    root.style.setProperty(
+      '--color-sunIcon',
+      colorMode === 'light'
+        ? '${COLORS.sunIcon.light}'
+        : '${COLORS.sunIcon.dark}'
+    );
     root.style.setProperty('--initial-color-mode', colorMode);
   })()
 `;
