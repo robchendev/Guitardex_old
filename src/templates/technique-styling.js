3314ce2aa542c7f0bbc2c2adaa0a8e1535c639f3
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-04-17 01:25:39 -0700

    cleaned technique template

--- src/templates/technique-styling.js
+++ src/templates/technique-styling.js
@@ -1,7 +1,54 @@
 import styled from "@emotion/styled"
 import { COLORS } from "../styles/globalstyles/theme"
-import { maxq } from "../styles/globalstyles/variables"
+import { v, maxq } from "../styles/globalstyles/variables"
 
+export const EmbedContainer = styled.div`
+  border-radius: ${v.borderRadius};
+  overflow: hidden;
+  position: relative;
+`
+export const HeadingContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: top;
+  h2 {
+    margin-bottom: 0.1em;
+  }
+  margin: 0 !important;
+`
+export const PageHeader = styled.div`
+  margin-bottom: 1em;
+`
+export const TechniqueName = styled.h2`
+  margin-bottom: 0;
+`
+export const ExerciseLinks = styled.ul`
+  margin-bottom: 0.5em;
+  li {
+    display: flex;
+    align-items: center;
+    margin: 5px 0;
+  }
+`
+export const EntireWrapper = styled.div`
+  .ttEdit {
+    padding: 3px 6px 4px 6px;
+    background-color: var(--color-text, ${COLORS.text.light});
+    color: var(--color-ttText, ${COLORS.ttText.light});
+    opacity: 1 !important;
+    font-size: 1rem;
+    max-width: 13em;
+    text-align: center;
+    transition: none;
+  }
+`
+export const SaveContainer = styled.div`
+  top: 0;
+  right: 0;
+  border-radius: 0 ${v.borderRadius} ${v.borderRadius} 0;
+  height: 3.5em;
+  width: 3.5em;
+`
 export const TechList = styled.div`
   a {
     border: none;
@@ -14,7 +61,6 @@ export const TechList = styled.div`
     color: var(--color-text, ${COLORS.text.light});
   }
 `
-
 export const DarkBackground = styled.div`
   padding: 1em;
   border-radius: 12px;
@@ -24,7 +70,6 @@ export const DarkBackground = styled.div`
   }
 `
 export const Explanation = styled.div`
-
   padding: 1.5em 0;
   border-radius: 12px;
   ${maxq[1]} {
@@ -47,7 +92,6 @@ export const Explanation = styled.div`
   h3:not(first-child),h4:not(first-child) {
     padding-top: 0.5em;
   }
-
 `
 export const VideoContainer = styled.div`
   margin-bottom: 0.5em;
