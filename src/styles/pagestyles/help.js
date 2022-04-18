842d019c925c10bbd4a27bdf7262e01b95f42d45
chendumpling99 <66714443+chendumpling99@users.noreply.github.com>
2022-04-18 01:19:12 -0700

    Updated help page

--- src/styles/pagestyles/help.js
+++ src/styles/pagestyles/help.js
@@ -1,8 +1,17 @@
 import styled from "@emotion/styled"
+import { css } from "@emotion/react"
 
-export const CustomOL = styled.ol`
+const sharedTraits = css`
   margin-left: 1em;
   li {
     margin: 3px 0;
   }
+`
+
+export const CustomOL = styled.ol`
+  ${sharedTraits}
+`
+
+export const CustomUL = styled.ul`
+  ${sharedTraits}
 `
